d4feea094ab90e606f554a3fe53fe4ab3d47766c
Bartek Zagórski <zagora171991@gmail.com>
2020-08-30 19:07:13 +0200

    add_feature_load_cattegories and refactor some code

---
+++ loadIncomesCattegories.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+require_once "dbconnect.php";
+
+$query = $dbConnection->prepare('SELECT name FROM `incomes_cattegories_assigned_to_users` WHERE user_id = :loggedID');
+$query -> bindValue(':loggedID', $_SESSION['loggedInUserId'], PDO::PARAM_INT);
+$query -> execute();
+
+$incomesCattegories = $query -> fetchAll();
+
+echo '<option value="" disabled selected hidden>Wybierz Kategorię</option>"';
+$iter = 1;
+foreach ($incomesCattegories as $inCat)
+{
+    echo '<option value="'.$iter++.'">'.$inCat['name'].'</option>';
+}
+?>
